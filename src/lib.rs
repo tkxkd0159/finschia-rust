9524dc80f06020a9627ee03b5fd4dafcaa4112e5
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-03-13 16:01:31 +0900

    impl converting `Any`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
 include!("prost/_include.rs");
+mod msg;
+
+pub use msg::{from_any, to_any};
 
 #[test]
-fn test_load_proto() {
-    let is_send = cosmos::bank::v1beta1::SendEnabled { denom: String::from("uatom"), enabled: true }.enabled;
-    assert!(is_send)
+fn test_proto_scheme() {
+    let is_send = cosmos::bank::v1beta1::SendEnabled {
+        denom: String::from("cony"),
+        enabled: true,
+    }
+    .enabled;
+    assert!(is_send);
+
+    let msg = cosmos::bank::v1beta1::MsgSend {
+        from_address: "me".to_string(),
+        to_address: "you".to_string(),
+        amount: vec![cosmos::base::v1beta1::Coin {
+            denom: "cony".to_string(),
+            amount: "100".to_string(),
+        }],
+    };
+
+    let anymsg = to_any(&msg, "/cosmos.bank.v1beta1.MsgSend");
+    assert_eq!(anymsg.type_url, "/cosmos.bank.v1beta1.MsgSend");
+    let origmsg = from_any::<cosmos::bank::v1beta1::MsgSend>(&anymsg).unwrap();
+    assert_eq!(msg, origmsg);
 }
---
+++ src/msg.rs
@@ -0,0 +1,19 @@
+use prost::{DecodeError, Message};
+use prost_types::Any;
+
+pub fn to_any<T>(msg: &T, type_url: &str) -> Any
+where
+    T: Message + Sized,
+{
+    Any {
+        type_url: type_url.to_string(),
+        value: msg.encode_to_vec(),
+    }
+}
+
+pub fn from_any<T>(msg: &Any) -> Result<T, DecodeError>
+where
+    T: Message + Sized + Default,
+{
+    T::decode(msg.value.as_slice())
+}
